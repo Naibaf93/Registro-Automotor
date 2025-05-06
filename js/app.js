f28ac8ce936d39c717bec7c9bef3b883e67a724c
Fabián Huertas <miloh9318@hotmail.com>
2025-05-06 11:29:19 -0500

    creando la variable automoviles que es un array que guarda las otras variables de automoviles

--- js/app.js
+++ js/app.js
@@ -9,6 +9,7 @@ function Automovil(marca, modelo,  color, anio, titular){
 let automovil1= new Automovil('Chevrolet', 'Camaro', 'Amarillo', 2017, 'Sam Witwicky');
 let automovil2 = new Automovil('Mazda', 'Allegro', 'Negro', 2022, 'Fabián Huertas');
 let automovil3 = new Automovil('Renault', 'Sandero', 'Gris', 2011, 'Antonio Valencia');
+let automoviles = [automovil1, automovil2, automovil3];
 
 Automovil.prototype.venderAutomovil = function(nuevoTitular){
     this.titular = nuevoTitular;
