fd32b7e7cec7e13237cf3bafd62fa98ed0b3eb70
Fabián Huertas <miloh9318@hotmail.com>
2025-05-06 11:04:19 -0500

    creando el constructor de automovil

---
+++ js/app.js
@@ -0,0 +1,7 @@
+function Automovil(marca, modelo,  color, anio, titular){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.color = color;
+    this.anio = anio;
+    this.titular = titular;
+}
